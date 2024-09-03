d4ef9a566be6d5bcbefe8985086749ebda9243bb
Livinalt <livinalt@gmail.com>
2024-09-03 06:51:12 +0100

    added Mixar Token

---
+++ contracts/MixarToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MixaToken is ERC20 {
+
+    address owner;
+
+    uint256 public constant maxTotalSupply = 50000000 * 10 ** 18;
+
+    constructor() ERC20("Mixar", "MXR") {}
+
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
+    }
+}
