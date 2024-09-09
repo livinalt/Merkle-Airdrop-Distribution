92dd660757b406e4adbd50221421d71a4052d67a
Livinalt <livinalt@gmail.com>
2024-09-09 14:05:11 +0100

    updating test script

--- test/MerkleAirdrop.ts
+++ test/MerkleAirdrop.ts
@@ -6,38 +6,41 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Lock", function () {
+describe("MerkleAirdrop", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    
+  async function MerkleAirdropFixture() {
     const mixarToken = "0xd28F5a6dB5EF6597eE7C644999e041E357A27a75";
     const MerkleRoot = "0x8741a333ebe4b3abbc7e896be9dbfce9b6e3792e1d0f5594f26b5fe8f3f075c2";
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+    const [owner, otherAccount, addr1, addr2] = await hre.ethers.getSigners();
 
     const MerkleAirdrop = await hre.ethers.getContractFactory("MerkleAirdrop");
     const merkleAirdrop = await MerkleAirdrop.deploy(mixarToken, MerkleRoot);
 
-    return { merkleAirdrop, mixarToken, MerkleRoot, owner, otherAccount };
+    return { merkleAirdrop, mixarToken, MerkleRoot, owner, addr1, addr2, otherAccount };
   }
 
   describe("Deployment", function () {
-   
     it("Should set the right owner", async function () {
-      const { merkleAirdrop, owner } = await loadFixture(deployOneYearLockFixture);
-
+      const { merkleAirdrop, owner } = await loadFixture(MerkleAirdropFixture);
       expect(await merkleAirdrop.owner()).to.equal(owner.address);
     });
-
   });
 
   describe("merkleTree", function () {
-    it("claim airdrop", function () {
+    it("Should claim airdrop", async function () {
+      const { merkleAirdrop, addr1, merkleTree } = await loadFixture(MerkleAirdropFixture);
 
-    })
+      const amount = "10000000000000000000";
+      const proof = merkleTree.getHexProof(keccak256(`${addr1.address}${amount}`));
+
+      await expect(merkleAirdrop.connect(addr1).claimAirdrop(amount, proof))
+        .to.emit(merkleAirdrop, "AirdropClaimed")
+        .withArgs(addr1.address, amount);
+    });
   });
 
   describe("withdrawTokens", function () {
@@ -51,73 +54,4 @@ describe("Lock", function () {
   describe("check qualification", function () {
 
   });
-  
-
-
-
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  // });
 });
