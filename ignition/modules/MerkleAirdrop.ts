baf4c8d78546e830002372b1251009c5869d5377
Livinalt <livinalt@gmail.com>
2024-09-09 12:15:11 +0100

    updated MerkelAirdrop.ts and eligibleList.csv

--- ignition/modules/MerkleAirdrop.ts
+++ ignition/modules/MerkleAirdrop.ts
@@ -1,10 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const MerkleAirdropModule = buildModule("LockModule", (m) => {
-  const tokenAddress = 0xB8044557BE5Ec91Eaa9eBD92101dCD116D62e6Cc;
-  const merkleRoot = ;
+const MerkleAirdropModule = buildModule("MerkleAirdropModule", (m) => {
 
-  const merkleAirdrop = m.contract("MerkleAirdrop", tokenAddress, merkleRoot);
+  const tokenAddress = "0xB8044557BE5Ec91Eaa9eBD92101dCD116D62e6Cc";
+  const merkleRoot = "0x8741a333ebe4b3abbc7e896be9dbfce9b6e3792e1d0f5594f26b5fe8f3f075c2";
+
+  const merkleAirdrop = m.contract("MerkleAirdrop", [tokenAddress, merkleRoot]);
 
   return { merkleAirdrop };
 });
--- test/MerkleAirdrop.ts
+++ test/MerkleAirdrop.ts
@@ -11,52 +11,27 @@ describe("Lock", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    
+    const mixarToken = "";
+    const MerkleRoot = "0x94c8ffd73b8c2dff39ecacdf98438e4f862b5b5e80d509bba5624693a21b611e";
 
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await hre.ethers.getSigners();
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const MerkleAirdrop = await hre.ethers.getContractFactory("MerkleAirdrop");
+    const merkleAirdrop = await MerkleAirdrop.deploy(mixarToken, MerkleRoot);
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { merkleAirdrop, mixarToken, MerkleRoot, owner, otherAccount };
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
+   
     it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+      const { merkleAirdrop, owner } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await lock.owner()).to.equal(owner.address);
+      expect(await merkleAirdrop.owner()).to.equal(owner.address);
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
   });
 
   describe("Withdrawals", function () {
